18b79efdeeb0a747625269e4f4a702120caa2b0e
Amanuel Negussie <Anegussie9077@conestogac.on.ca>
2023-03-18 14:55:12 -0400

    Attempted to add button

---
+++ init.js
@@ -0,0 +1,19 @@
+
+(function() {
+
+    function fnAddButtons(){
+        var btn = document.createElement("input"); 
+        btn.value = "Optimize"; 
+        btn.id = "optimize-mm-btn"; 
+        btn.type = "submit";
+        //btn.className = "oya4hc vxq1Hc"
+        let myDiv = document.createElement("div");
+        let element = document.querySelector(".FkdJRd.vRIAEd.dS8AEf");
+        myDiv.appendChild(btn);
+        element.appendChild(myDiv);
+    }
+
+  
+    fnAddButtons();
+
+})();
