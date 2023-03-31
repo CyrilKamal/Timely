a8186d48f505b205f34cdf2d4a8eedf13837c31e
Amanuel <anegussie9077@conestogac.on.ca>
2023-03-31 11:15:04 -0400

    Time Wallet 1.0 Implemented
    - incorporates time saved as well with bolder look

--- content.js
+++ content.js
@@ -134,6 +134,63 @@ function addTime(totalTime, timeToAdd) {
 }
 
 
+// using a get function to receive the total time, then passing total time into this function to receive the currency
+function getCurrency(totalTime, avg_speed_km_h = 60, fuel_consumption_l_100km = 10, avg_gas_price_per_l = 1.50) {
+    const totalPattern = /(\d{3}) day (\d{2}) hr (\d{2}) min/;
+    const addPattern = /(\d{1,2}) hr (\d{1,2}) min/;
+    const totalMatch = totalPattern.exec(totalTime);
+    const addMatch = addPattern.exec(totalTime);
+
+    let totalMinutesWithDays;
+   
+
+    if (totalMatch === null) {
+        //at this point we know we're adding current time and not total time, if it's null here then it's a bad totalTime
+        if (addMatch ==null){
+
+            throw new Error(`Invalid totalTime: ${totalTime}`);
+        }else{
+
+         
+            const addHours = parseInt(addMatch[1]);
+            const addMinutes = parseInt(addMatch[2]);
+            
+            totalMinutesWithDays = addMinutes + addHours * 60;
+
+        }
+         
+       
+    }else{
+
+
+    const totalDays = parseInt(totalMatch[1]);
+    const totalHours = parseInt(totalMatch[2]);
+    const totalMinutes = parseInt(totalMatch[3]);
+
+
+    totalMinutesWithDays = totalMinutes + totalHours * 60 + totalDays * 24 * 60;
+    }
+
+    // total minutes With Days //minutes to money --> assumption 60km/h 1km/minute 10L/100km 1L/1km --> 1L/minute $1.50 per L 
+
+    // totalMinutes --> 1km/minute --> 1L/minute --> $1.50/L --> money 
+
+    // variables --> average speed (km/h) --> fuel consumption (L/100km) --> gas-price ($ / per L)
+    // 60km/hour --> /60 --> km/min
+    let km_per_m = avg_speed_km_h / 60;
+    //km/minute L/100km  / 100
+    let l_per_m = km_per_m * fuel_consumption_l_100km / 100;
+
+    // l/m * $/l  = $/m 
+    let money_per_minute = l_per_m * avg_gas_price_per_l;
+
+    let money = money_per_minute * totalMinutesWithDays;
+
+
+
+    return money.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+}
+
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
     if (request.action === "urlUpdated") {
         globalUrl = request.url;
@@ -177,9 +234,15 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
         }
 
 
-        const message = `You just saved ${request.timeSaved.replace(/"/g, '')} of travel time!`;
-        showPopup(message, '#4BB543');
-        showPopup("Timely has saved you a total of: \n" + total.previous_total, '#4c8bf5')    
+
+
+        const message = `You just saved ${request.timeSaved.replace(/"/g, '')} of travel time!\nYou also saved ${getCurrency(request.timeSaved)}`;
+
+
+        showPopup(message, '#4BB543', 'timely now');
+        showPopup("Congratulations! Saving the world one trip at a time!\n\n" +
+            "Total Time Saved with Timely: " + total.previous_total
+            + "\nTotal Money Saved with Timely: " + getCurrency(total.previous_total), '#4c8bf5', 'timely wallet')
     }
 });
 
@@ -305,7 +368,7 @@ function retryText(url) {
 
 var popupHeight = 10; // Initialize popup height to 0
 
-function showPopup(errorMessage, backgroundColor) {
+function showPopup(errorMessage, backgroundColor, title, font_size = 16) {
     var popup = document.createElement('div');
     popup.setAttribute('id', 'error-popup');
     popup.setAttribute('style', 'position: fixed; top: ' + popupHeight + 'px; right: 10px; z-index: 1000; background-color: ' + backgroundColor + '; color: white; padding: 16px; border-radius: 4px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);');
@@ -318,16 +381,27 @@ function showPopup(errorMessage, backgroundColor) {
     });
 
     var message = document.createElement('span');
-    message.setAttribute('style', 'display: inline-block; vertical-align: middle;');
+    message.setAttribute('style', 'display: inline-block; vertical-align: middle; font-size: ' + font_size + 'px;');
     message.innerText = errorMessage;
 
     popup.appendChild(closeButton);
-    popup.appendChild(message);
+    if (title != undefined) {
+        var titleMessage = document.createElement('span');
+        font_size += 12;
+        titleMessage.setAttribute('style', 'display: inline-block; vertical-align: middle; font-size: ' + font_size + 'px; font-weight: bold');
+        titleMessage.innerText = title + '\n';
+        titleMessage.append(message)
+        popup.appendChild(titleMessage)
+    } else {
+        popup.appendChild(message);
+    }
+
+
     document.body.appendChild(popup);
     popupHeight += popup.offsetHeight + 10; // Add the height of the popup and some margin to the total popup height
     setTimeout(() => {
         popup.remove();
-    }, 5000);
+    }, 10000);
 }
 
 function AddRouteButton() {
